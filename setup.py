f3674438292be53d9155702a718970788a5e32ec
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-03-25 10:37:46 -0500

    make uses by default 2 cores

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 import os, sys, shutil
 import glob
 from distutils.command.build import build
-from distutils.command.build_py import build_py
+from setuptools.command.build_py import build_py
 from distutils.command.clean import clean
 
 class RDKitBuild(build):
@@ -43,7 +43,7 @@ class RDKitBuild(build):
       import ninja
     except:
       self.spawn(cmd=['cmake', '..'] +  cm_args)
-      self.spawn(cmd=['make', 'install'])
+      self.spawn(cmd=['make', '-j2', 'install'])
     else:
       self.spawn(cmd=['cmake', '-GNinja', '..'] +  cm_args)
       self.spawn(cmd=['ninja'])
@@ -67,7 +67,7 @@ class RDKitBuild_py(build_py):
 
     python_lib = self.find('libpython{}.so'.format(python_version).format(python_version), '/') 
     python_exec = sys.executable
-
+    
     if not python_lib:
       print('Could not find any installed python-dev (libpython.so) library.')
       print('Proceeding ...')
@@ -80,7 +80,7 @@ class RDKitBuild_py(build_py):
       import ninja
     except:
       self.spawn(cmd=['cmake', '..'] +  cm_args)
-      self.spawn(cmd=['make', 'install', '-j2'])
+      self.spawn(cmd=['make', 'install'])
     else:
       self.spawn(cmd=['cmake', '-GNinja', '..'] +  cm_args)
       self.spawn(cmd=['ninja'])
@@ -92,7 +92,7 @@ class RDKClean(clean):
 
   def delObjFiles(self, fdir):
 
-    dfile_ext = ['tar.gz', '.so.1', '.so', '.log']
+    dfile_ext = ['tar.gz', '.so.1', '.so', '.log', '.a']
 
     for path in glob.glob(fdir):
       if os.path.isdir(path):
@@ -108,14 +108,15 @@ class RDKClean(clean):
     dlist = ['build', 'External/rapidjson-1.1.0', 'External/catch/catch/',
             'External/YAeHMOP/tmp/', 'External/YAeHMOP/src/',
             'External/CoordGen/maeparser/', 'External/CoordGen/coordgen/',
-            'External/YAeHMOP/yaehmop/',
+            'External/YAeHMOP/yaehmop/', 'lib',
             'rdkit/Chem/inchi.py', 'Code/GraphMol/SLNParse/lex.yysln.cpp',
             'Code/GraphMol/SLNParse/sln.tab.cpp', 'Code/GraphMol/SLNParse/sln.tab.hpp',
             'Code/GraphMol/SmilesParse/lex.yysmarts.cpp', 'Code/GraphMol/SmilesParse/lex.yysmiles.cpp',
             'Code/GraphMol/SmilesParse/smarts.tab.cpp', 'Code/GraphMol/SmilesParse/smarts.tab.hpp',
             'Code/GraphMol/SmilesParse/smiles.tab.cpp', 'Code/GraphMol/SmilesParse/smiles.tab.hpp',
             'Code/RDGeneral/RDConfig.h', 'Code/RDGeneral/export.h', 'Code/RDGeneral/test.h', 
-            'Code/RDGeneral/versions.cpp', 'Code/RDGeneral/versions.h']
+            'Code/RDGeneral/versions.cpp', 'Code/RDGeneral/versions.h', 'rdkit/RDPaths.py',
+            'Data/eht_parms.dat', 'Data/templates.mae']
 
     for dobj in dlist:
       path = os.path.join(fdir, dobj)
@@ -133,7 +134,7 @@ class RDKClean(clean):
     self.delDownloadedFiles('src')
     super().run()
 
-def pre_build():
+def pre_build(include_dirs):
 
   if not os.path.isdir("src/build"):
     os.mkdir('src/build')
@@ -141,9 +142,13 @@ def pre_build():
   if not os.path.isdir("src/build/lib"):
     os.mkdir('src/build/lib')
 
+  for include_dir in include_dirs:
+    open(include_dir + '/__init__.py', 'a').close()
+
 if __name__ == '__main__':
 
-  pre_build() # must be always called to ensure sdist is built correctly
+  include_dirs = [] #'src/lib', 'src/Data', 'src/Projects', 'src/Contrib', 'src/Docs']
+  pre_build(include_dirs) # must be always called to ensure sdist is built correctly
 
   setuptools.setup(
       name = "rdkit",
@@ -152,9 +157,8 @@ if __name__ == '__main__':
       license = "BSD",
       keywords = "cheminformatics",
       url = "https://github.com/rdkit/rdkit",
-      packages=setuptools.find_packages('src') + ['rdkit.lib', 'rdkit.Data', 'rdkit.Projects', 'rdkit.Contrib', 'rdkit.Docs'] ,
-      package_dir={'rdkit':'src/rdkit', 'rdkit.lib':'src/build/lib', 'rdkit.Data': 'src/Data', 'rdkit.Docs': 'src/Docs', \
-                  'rdkit.Projects': 'src/Projects', 'rdkit.Contrib': 'src/Contrib'},
+      packages=setuptools.find_packages('src'),
+      package_dir={'rdkit': 'src/rdkit', 'lib': 'src/lib', 'Data': 'src/Data', 'Projects': 'src/Projects', 'Docs': 'src/Docs', 'Contrib': 'src/Contrib'},
       include_package_data=True,
       classifiers=[
           "Topic :: Utilities",
